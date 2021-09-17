6db26f73135951ea86921c0658a41644c8f2dba9
Fatimata <38009859@parisnanterre.fr>
2021-09-17 17:01:09 +0200

    Installation.1

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import React, { Component, Fragment, useEffect, useState } from "react";
 
 function Monapp() {
   return(
+    <center>
     <div>
-      test
+      <h1> Bienvenue sur perdUPN </h1>
+      <h2> Un objet perdu ? Trouvé ? perdUPN est fait pour vous </h2>
+
+      <button type="button" class="btn btn-info">Info</button>
+
+
     </div>
-    
+    </center>
+
   )
 }
 export default Monapp;
